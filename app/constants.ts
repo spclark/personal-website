296695994b868cebb3227dd159dc01cc399e51c9
Steven Clark <55724740+spclark@users.noreply.github.com>
2026-01-18 22:37:12 -0500

    Add constants

---
+++ app/constants.ts
@@ -0,0 +1,14 @@
+export const EMAIL = 'emailme@stevenclark.xyz';
+
+export const GITHUB_URL = 'https://github.com/spclark';
+
+export const LINKEDIN_URL =
+	'https://www.linkedin.com/in/steven-clark-14260318b';
+
+export const ROUTES = {
+	ABOUT: '/about',
+	HOME: '/',
+	NOT_FOUND: '/not-found',
+	PROTEC: '/protec-system-one',
+	TRIPPED_UP: '/tripped-up',
+};
