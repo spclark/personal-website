af11272995e6ac2abe4e1be98af1ef47f63a788f
Steven Clark <55724740+spclark@users.noreply.github.com>
2026-01-25 19:47:48 -0500

    Add Pro-Tec project description

--- app/routes/projects/protec.tsx
+++ app/routes/projects/protec.tsx
@@ -2,7 +2,11 @@ import Window from '~/components/Window';
 
 const ProTec = () => (
 	<Window title="Pro-Tec System One GPS Tracker">
-		<p>Description coming soon!</p>
+		<p>
+			A web application enabling real-time tracking, event alerting, and
+			arming/disarming of Pro-Tec System One GPS units. Built using Vue and
+			Fastify.
+		</p>
 		<br />
 		<p>
 			You can visit the site{' '}
